bc02a0eea9e21788d88a8b06e9482aa81e53a41c
M4ster <www.m4ster@gmail.com>
2018-10-06 18:29:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# find-subdomains
+
+Do you miss AXFR technique? This tool allows to get the subdomains from a HTTP**S** website in a few seconds.  
+How it works? find-subdomains does not use neither dictionary attack nor brute-force, it just abuses of Certificate Transparency logs.  
+For more information about CT logs, check www.certificate-transparency.org and [crt.sh](https://crt.sh/).
+
+## Getting Started
+Please, follow the instructions below for installing and run find-subdomains.
+
+### Pre-requisites
+Make sure you have installed the following tools:
+```
+# Installing Node.js via package manager
+https://nodejs.org/en/download/package-manager/
+# Install Yarn (Node.js dependency management tools, Like python pip)
+https://yarnpkg.com/en/docs/install
+```
+
+### Installing
+```bash
+# 1. Global install
+$ yarn global add find-subdomains
+# 1.1 Running
+$ find-subdomains github.com
+
+# 2. Clone from GitHub
+$ git clone https://github.com/monkeym4ster/find-subdomains.git
+$ cd find-subdomains
+$ yarn install
+# 2.1 Running
+$ node find-subdomains.js github.com
+```
+
+### Running
+```bash
+$ node find-subdomains.js --help
+```
+
+## Thanks
+* *Sheila A. Berta - [(@UnaPibaGeek)](https://www.twitter.com/UnaPibaGeek).*
---
+++ find-subdomains.js
@@ -0,0 +1,57 @@
+const request = require('superagent')
+const url = require('url')
+const fs = require('fs')
+const pkg = require('./package.json')
+
+const domain = 'knownsec.com'
+
+const getRawResponse = domain => {
+  return new Promise((resolve, reject) => {
+    request.get(`https://crt.sh/?q=%.${domain}&output=json`)
+      .then(res => resolve(res.text))
+      .catch(err => {
+        if (err.statusCode !== 200) return reject(err)
+        return resolve(err.rawResponse)
+      })
+  })
+}
+
+const printBanner = () => {
+  const fileData = fs.readFileSync('./banner.txt', 'utf8')
+  console.log(`${fileData} Version ${pkg.version}\n`)
+
+}
+
+const clearUrl = target => {
+  let domain = target
+  if (/^(https?:|www\.)/i.test(target)) domain = url.parse(target).hostname || domain
+  // domain = domain.replace(/.*www\./i, '') // TODO: not need multiple replace
+  return domain
+}
+
+;(async () => {
+  printBanner()
+
+  if (process.argv.length !== 3) {
+    console.log(`usage: find-subdomains DOMAIN`)
+    process.exit(1)
+  }
+
+  const target = process.argv[2]
+  const domain = clearUrl(target)
+  
+  console.log(`[*] TARGET: ${domain}\n`)
+
+  let response
+  try { 
+    response = await getRawResponse(domain)
+  } catch (err) {
+    console.log(`[X] Information not available! (${err.statusCode})`)
+    return process.exit(1)
+  }
+
+  const jsonData = JSON.parse(`[${response.trim().replace(/}{/g, '},{')}]`)
+  const subdomains = Array.from(new Set(jsonData.map(_ => _.name_value)))
+  for (const _ of subdomains) console.log(`[+] ${_}`)
+  console.log('\n[*]  Done. Have a nice day! ;).')
+})()
